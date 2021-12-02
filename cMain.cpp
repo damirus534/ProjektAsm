3325876baf4c4af155e9c085e9615dd955ab03d5
damirus534 <damianrussok@gmail.com>
2021-12-02 20:17:56 +0100

    Utworzenie Gui cz.1

--- cMain.cpp
+++ cMain.cpp
@@ -1,9 +1,54 @@
 #include "cMain.h"
 
-cMain::cMain() : wxFrame(nullptr, wxID_ANY, "ProjektAsm") {
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+		EVT_SLIDER(22, onSliderChange)
+
+wxEND_EVENT_TABLE()
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "ProjektAsm", wxPoint(560, 240),wxSize(800,600)) {
+
+	//przyciski
+	inputFileButton = new wxButton(this, wxID_ANY, "Wgraj plik", wxPoint(10, 10), wxSize(120, 30));
+	openOutputFileButton = new wxButton(this, wxID_ANY, "Otworz plik", wxPoint(340, 10), wxSize(120, 30));
+	run = new wxButton(this, wxID_ANY, "Uruchom", wxPoint(630, 500), wxSize(120, 30));
+
+	//checkboxy
+	cppCheckBox = new wxCheckBox(this, wxID_ANY, "c++", wxPoint(10, 120), wxSize(60, 15));
+	asmCheckBox = new wxCheckBox(this, wxID_ANY, "asm", wxPoint(10, 140), wxSize(60, 15));
+	cppCheckBox->SetFont(myFont);
+	asmCheckBox->SetFont(myFont);
+
+
+	//slider
+	slider = new wxSlider(this, 22, 1, 1, 64, wxPoint(40, 320), wxSize(420, 25));
+
+	//labele stale
+	cppLabel = new wxStaticText(this, wxID_ANY, "c++", wxPoint(250, 120), wxSize(25, 15));
+	asmLabel = new wxStaticText(this, wxID_ANY, "asm", wxPoint(250, 140), wxSize(25, 15));
+	avarageTimeLabel = new wxStaticText(this, wxID_ANY, "avarage time", wxPoint(300, 100), wxSize(100, 15));
+	previousTimeLabel = new wxStaticText(this, wxID_ANY, "previous time", wxPoint(400, 100), wxSize(100, 15));
+	oneOnSlider = new wxStaticText(this, wxID_ANY, "1", wxPoint(30, 325), wxSize(10, 20));
+	sixtyFourOnSlider = new wxStaticText(this, wxID_ANY, "64", wxPoint(460, 325), wxSize(20, 20));
+
+	//labele zmienne
+	currentValueOnSlider = new wxStaticText(this, wxID_ANY, "1", wxPoint(240, 345), wxSize(20, 20));
+
+	//ustawienie fonta w labelach
+	cppLabel->SetFont(myFont);
+	asmLabel->SetFont(myFont);
+	avarageTimeLabel->SetFont(myFont);
+	previousTimeLabel->SetFont(myFont);
+
 
 }
 
 cMain::~cMain() {
 
+}
+
+void cMain::onSliderChange(wxCommandEvent& evt) {
+	int currentValueInt = slider->GetValue();
+	wxString currentValueWxString;
+	currentValueWxString << currentValueInt;
+	currentValueOnSlider->SetLabelText(currentValueWxString);
 }
--- cMain.h
+++ cMain.h
@@ -3,11 +3,57 @@
 
 class cMain : public wxFrame
 {
+private:
+	wxFont myFont = wxFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+
 public:
+
+	wxFont myFontGetter() {
+		return this->myFont;
+	}
+
+
+
+	//constructor
 	cMain();
+
+
+	//destructor
 	~cMain();
 
+public:
+	//Deklaracja przycisków
+	wxButton* inputFileButton = nullptr; //Przycisk do wgrania pliku tekstowego
+	wxButton* openOutputFileButton = nullptr; //Przycisk do otworzenia pliku tekstowego z wynikami
+	wxButton* run = nullptr; //Przycisk do uruchomienia i wygenerowania pliku z wynikami
 
+	//Deklaracja checkboxów (asm, c++)
+	wxCheckBox* cppCheckBox = nullptr;
+	wxCheckBox* asmCheckBox = nullptr;
+
+	//labele stale
+	wxStaticText* cppLabel = nullptr;
+	wxStaticText* asmLabel = nullptr;
+	wxStaticText* avarageTimeLabel = nullptr;
+	wxStaticText* previousTimeLabel = nullptr;
+	wxStaticText* oneOnSlider = nullptr;
+	wxStaticText* sixtyFourOnSlider = nullptr;
+
+	//labele zmienne
+	wxStaticText* inputFileNameLabel = nullptr;
+	wxStaticText* outputFileNameLabel = nullptr;
+	wxStaticText* cppAvarageTimeLabel = nullptr;
+	wxStaticText* cppLastTimeLabel = nullptr;
+	wxStaticText* asmAvarageTimeLabel = nullptr;
+	wxStaticText* asmLastTimeLabel = nullptr;
+	wxStaticText* currentValueOnSlider = nullptr;
+
+	//slider
+	wxSlider* slider = nullptr;
+
+	void onSliderChange(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
 
 };
 
